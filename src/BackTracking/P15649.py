292e35890c8a8c9020683e447752426ea21e6352
skyriv213 <gn1007@dankook.ac.kr>
2022-08-18 02:55:16 +0900

    알고리즘 학습

---
+++ src/BackTracking/P15649.py
@@ -0,0 +1,20 @@
+n, m = map(int, input().split())
+
+s = []
+
+# 메서드
+def dfs():
+    # 배열의 길이가 m이 되는 경우 해당 리스트 내의 숫자들을 map으로 join하여 출력 후 해당 dfs 종료
+    if len(s) == m:
+        print(" ".join(map(str, s)))
+        return
+
+    # for 문으로 반복
+    for i in range(1, n + 1):
+        # 만약 i가 리스트 s안에 존재하지 않는다면 삽입을 해주고 dfs를 호출한다.
+        # 해당 dfs가 출력 된 후 가장 뒤에 들어온 i를 pop하여 제거해준다.
+        if i not in s:
+            s.append(i)
+            dfs()
+            s.pop()
+
--- src/BackTracking/P15650.py
+++ src/BackTracking/P15650.py
@@ -2,3 +2,6 @@
 1부터 n까지 자연수 중에 중복없이 m개
 '''
 
+n, m = map(int, input().split())
+s = []
+
