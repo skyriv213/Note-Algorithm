41bf4aaf5aede62d9bc614f4454b720394a71926
skyriv213 <gn1007@dankook.ac.kr>
2022-10-06 17:42:31 +0900

    이것이 코딩테스트다  -  구현 / 문자열 문제풀이

---
+++ src/String/Book/P08.py
@@ -0,0 +1,17 @@
+s = input()
+
+arr = []
+sum = 0
+for i in s:
+    if i.isdigit():
+        sum +=  int(i)
+    else:
+        arr.append(i)
+
+arr.sort()
+
+for i in arr:
+    print(i, end="")
+
+if sum != 0:
+    print(sum)
