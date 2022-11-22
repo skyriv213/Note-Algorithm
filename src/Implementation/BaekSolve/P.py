ce548491e0dab3803a0c829ba0b03fdfd2e023a2
skyriv213 <gn1007@dankook.ac.kr>
2022-11-22 22:21:40 +0900

    GCD 최소공약수 하는 알고리즘

---
+++ src/Implementation/BaekSolve/P.py
@@ -0,0 +1,11 @@
+# def GCD(p, q):
+#     if q == 0:
+#         return p
+#     return GCD(q, p % q)
+#
+#
+# n, m = map(int, input().split())
+# a = int(str(1) * n)
+# b = int(str(1) * m)
+# print(GCD(a, b))
+
