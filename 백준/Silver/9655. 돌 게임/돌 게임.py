c45567f9566835e522f6bddb0b8aaf2ba9347cb1
skyriv213 <80723571+skyriv213@users.noreply.github.com>
2024-09-21 11:06:50 +0900

    [Silver V] Title: 돌 게임, Time: 32 ms, Memory: 31252 KB -BaekjoonHub

--- 백준/Silver/9655. 돌 게임/돌 게임.py
+++ 백준/Silver/9655. 돌 게임/돌 게임.py
@@ -1,18 +1,32 @@
+'''
+돌 게임은 두 명이서 즐기는 재밌는 게임이다.
+
+탁자 위에 돌 N개가 있다. 상근이와 창영이는 턴을 번갈아가면서 돌을 가져가며,
+
+돌은 1개 또는 3개 가져갈 수 있다. 마지막 돌을 가져가는 사람이 게임을 이기게 된다.
+
+두 사람이 완벽하게 게임을 했을 때, 이기는 사람을 구하는 프로그램을 작성하시오. 게임은 상근이가 먼저 시작한다.
+'''
+
 n = int(input())
 
-win = [-1]*10001
-
-win[1] = 1 #SK
-win[2] = 0 #CY
-win[3] = 1 #SK
-
-for i in range(4,n+1):
-    if win[i-1] == 1 or win[i-3] == 1:
-        win[i] = 0
+# if n%2 ==0:
+#     print("CY")
+# else:
+#     print("SK")
+if 1 <= n < 3:
+    if n == 1:
+        print("SK")
     else:
-        win[i] = 1
-
-if win[n]==1:
-    print('SK')
+        print("CY")
 else:
-    print('CY')
+    memo = [1e9 for i in range(n + 1)]
+    memo[1] = 1
+    memo[2] = 0
+    for i in range(3, n + 1):
+        memo[i] = memo[i - 2]
+    
+    if memo[n] == 0:
+        print("CY")
+    else:
+        print("SK")
