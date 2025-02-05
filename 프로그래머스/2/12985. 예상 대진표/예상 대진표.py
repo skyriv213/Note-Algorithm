1d5201bf62b98abe450bbcd10d399f5266da242a
skyriv213 <80723571+skyriv213@users.noreply.github.com>
2025-02-05 11:15:49 +0900

    [level 2] Title: 예상 대진표, Time: 0.02 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/2/12985. 예상 대진표/예상 대진표.py
+++ 프로그래머스/2/12985. 예상 대진표/예상 대진표.py
@@ -1,12 +1,15 @@
-import math
 def solution(n,a,b):
-    answer = 1
+    answer=0
+    while a!=b:
+        if a%2!=0:
+            a+=1
+        a//=2
+        if b%2!=0:
+            b+=1
+        b//=2    
+        # if a//2 == b//2:
+        #     return answer
+        answer=+1
+    
 
-    while a!=b :
-        a = math.ceil(a/2)
-        b = math.ceil(b/2)
-        answer+=1
-        if a==b :
-            answer-=1
-        
     return answer
