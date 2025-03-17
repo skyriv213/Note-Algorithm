704ae45604a908a72685e86e5955bfc3202c4b55
skyriv213 <80723571+skyriv213@users.noreply.github.com>
2025-03-17 21:37:02 +0900

    [level 0] Title: 할 일 목록, Time: 0.05 ms, Memory: 82.8 MB -BaekjoonHub

---
+++ 프로그래머스/0/181885. 할 일 목록/할 일 목록.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+class Solution {
+    public String[] solution(String[] todo_list, boolean[] finished) {
+        ArrayList<String> answer = new ArrayList<>();
+        for(int i = 0; i<finished.length;i++){
+            if(!finished[i]){
+                answer.add(todo_list[i]);
+            }
+        }
+        return answer.toArray(new String[0]);
+    }
+}
