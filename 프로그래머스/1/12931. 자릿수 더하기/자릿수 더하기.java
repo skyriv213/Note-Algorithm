4ec982067e44858125e7a29b1fead38d0b24941e
skyriv213 <80723571+skyriv213@users.noreply.github.com>
2025-04-15 11:32:48 +0900

    [level 1] Title: 자릿수 더하기, Time: 0.19 ms, Memory: 82.7 MB -BaekjoonHub

---
+++ 프로그래머스/1/12931. 자릿수 더하기/자릿수 더하기.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+public class Solution {
+    public int solution(int n) {
+        int answer = 0;
+        String [] s = String.valueOf(n).split("");
+        for(int i = 0; i<s.length;i++){
+            answer = answer + Integer.parseInt(s[i]);
+        }
+
+        return answer;
+    }
+}
