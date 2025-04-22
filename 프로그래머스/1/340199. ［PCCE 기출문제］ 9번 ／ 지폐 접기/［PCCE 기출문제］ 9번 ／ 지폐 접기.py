409387e313a0f784f52684f14081d1246efe91df
skyriv213 <80723571+skyriv213@users.noreply.github.com>
2025-04-22 11:16:05 +0900

    [level 1] Title: [PCCE 기출문제] 9번 / 지폐 접기, Time: 0.01 ms, Memory: 9.16 MB -BaekjoonHub

---
+++ 프로그래머스/1/340199. ［PCCE 기출문제］ 9번 ／ 지폐 접기/［PCCE 기출문제］ 9번 ／ 지폐 접기.py
@@ -0,0 +1,9 @@
+def solution(wallet, bill):
+    answer = 0
+    while min(wallet)< min(bill) or max(wallet)<max(bill):
+        if bill[1]<bill[0]:
+            bill[0] //=2
+        else:
+            bill[1] //=2
+        answer+=1
+    return answer
