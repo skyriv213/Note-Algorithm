9a7ef2508c838a81d976330642cb9de93d1d964a
skyriv213 <80723571+skyriv213@users.noreply.github.com>
2025-06-27 21:42:01 +0900

    [level 1] Title: [PCCE 기출문제] 10번 / 공원, Time: 1.24 ms, Memory: 9.23 MB -BaekjoonHub

--- 프로그래머스/1/340198. ［PCCE 기출문제］ 10번 ／ 공원/［PCCE 기출문제］ 10번 ／ 공원.py
+++ 프로그래머스/1/340198. ［PCCE 기출문제］ 10번 ／ 공원/［PCCE 기출문제］ 10번 ／ 공원.py
@@ -1,29 +1,8 @@
 def solution(mats, park):
-    answer = -1
-    n = len(park)
-    m = len(park[0])
-    board = [[0]*m for _ in range(n)]
-    
+    answer = 0
     for i in range(len(park)):
         for j in range(len(park[0])):
-            if park[i][j] =="-1":
-                board[i][j] = 1
-            else:
-                board[i][j] =0
-    
-    dp = [[0]*m for _ in range(n)]
-    max_size = 0
-    for i in range(len(park)):
-        for j in range(len(park[0])):
-            if board[i][j] == 1:
-                if i == 0 or j ==0:
-                    dp[i][j] = 1
-                else:
-                    dp[i][j] = min(dp[i-1][j], dp[i][j-1], dp[i-1][j-1]) + 1           
-                max_size = max(max_size, dp[i][j])
-    
-    mats.sort(reverse = True)
-    for i in mats:
-        if i <= max_size:
-            return i
+            if park[i][j] == "-1":
+                park[i][j] = min(map(int, [park[i-1][j],park[i][j-1],park[i-1][j-1]]))+1
+    print(park)
     return answer
