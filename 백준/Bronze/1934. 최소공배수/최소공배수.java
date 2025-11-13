493d1f9cfb9b5fc995eb944a8c76ce21dcf850ed
skyriv213 <80723571+skyriv213@users.noreply.github.com>
2025-11-13 18:19:33 +0900

    [Bronze I] Title: 최소공배수, Time: 348 ms, Memory: 22392 KB -BaekjoonHub

---
+++ 백준/Bronze/1934. 최소공배수/최소공배수.java
@@ -0,0 +1,26 @@
+import java.util.Scanner; 
+
+public class Main {
+	static int GCD(int a, int b) {
+		int r = a%b;
+		if(r==0) {
+			return b;
+		}
+		else {
+			return GCD(b,r);
+		}
+	}
+	public static void main(String [] args) {
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		for(int i = 0 ; i < n;i++) {
+			int a = sc.nextInt();
+			int b = sc.nextInt();
+			int x = GCD(a,b);
+			
+			System.out.println(a*b/x);
+			
+		}
+	}
+}
+
